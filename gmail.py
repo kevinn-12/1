b719fa4992a29f8d4653da53dc034f21396fa6be
kevinn-12 <vdigueslacerda@gmail.com>
2020-09-03 11:37:17 +0200

    Fixed getting multiple dates

--- gmail.py
+++ gmail.py
@@ -15,7 +15,7 @@ def mail():
         creds = tools.run_flow(flow, store)
     service = build('gmail', 'v1', http=creds.authorize(Http()))
 
-    # Call the Gmail API to fetch INBOX
+    # Filter
     querry = "from:no-reply@tilburguniversity.edu is:unread subject:Tilburg University Sports Center Online booking"
 
     results = service.users().messages().list(userId='me',labelIds = ['INBOX'], q = querry).execute()
@@ -24,14 +24,18 @@ def mail():
     if not messages:
         print("No messages found.")
     else:
+        date_time_list = []
         for message in messages:
-            service.users().messages().modify(userId='me', id=message['id'], body={'removeLabelIds': ['UNREAD']}).execute()
+            # Marking as unread
+            # service.users().messages().modify(userId='me', id=message['id'], body={'removeLabelIds': ['UNREAD']}).execute()
+            # Getting Time
             msg = service.users().messages().get(userId='me', id=message['id']).execute()
             date_time = re.findall("\d{2}[-/]\d{2}[-/]\d{4}\s\d{2}[:/]\d{2}", str(msg))
-            start = datetime.strptime(date_time[0], '%d-%m-%Y %H:%M').isoformat()
-            end = datetime.strptime(date_time[1], '%d-%m-%Y %H:%M').isoformat()
-            # print(start, end)
-            return start, end
-print(mail())
-# if __name__ == '__main__':
-#     mail()
+            date_time_list.append(datetime.strptime(date_time[0], '%d-%m-%Y %H:%M').isoformat())
+            date_time_list.append(datetime.strptime(date_time[1], '%d-%m-%Y %H:%M').isoformat())
+            # print(date_time_list)
+        return date_time_list
+
+# print(mail())
+if __name__ == '__main__':
+    mail()
